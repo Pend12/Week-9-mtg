92a2b616bbee1da9985b4d764afce45d7c511a84
Pend12 <95454604+Pend12@users.noreply.github.com>
2023-11-10 10:03:28 +0700

    Add files via upload

---
+++ Week 09/Week 09/Week_09App.swift
@@ -0,0 +1,17 @@
+//
+//  Week_09App.swift
+//  Week 09
+//
+//  Created by MacBook Pro on 10/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Week_09App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
